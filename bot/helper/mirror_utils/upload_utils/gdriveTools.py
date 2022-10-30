8c178ee53c2b906f12847581e79b591000ef23d9
MikeTaven <116861577+MikeTaven@users.noreply.github.com>
2022-10-30 16:18:28 -0500

    Update gdriveTools.py

--- bot/helper/mirror_utils/upload_utils/gdriveTools.py
+++ bot/helper/mirror_utils/upload_utils/gdriveTools.py
@@ -149,7 +149,7 @@ class GoogleDriveHelper:
         # File body description
         file_metadata = {
             'name': file_name,
-            'description': 'Uploaded by Helios-Mirror-Bot',
+            'description': '',
             'mimeType': mime_type,
         }
         if parent_id is not None:
@@ -409,7 +409,7 @@ class GoogleDriveHelper:
     def __create_directory(self, directory_name, parent_id):
         file_metadata = {
             "name": directory_name,
-            "description": "Uploaded by Helios-mirror-bot",
+            "description": "",
             "mimeType": self.__G_DRIVE_DIR_MIME_TYPE
         }
         if parent_id is not None:
